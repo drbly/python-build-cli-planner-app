c001ebdb13ef465be5b7d9bc383b2882591e75a5
Dakota Bly <dakota.bly@kinsaleins.com>
2021-08-23 14:53:37 -0400

    bugfixes

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -4,7 +4,7 @@ from dateutil.parser import parse
 from datetime import datetime
 
 
-class DeadlineMetaReminder(Iterable, metaclass=ABCMeta):
+class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
 
     @abstractmethod
     def is_due(self):
